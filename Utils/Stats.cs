f83c7129587906e519febaa5b0f2ae720f3126f2
saumitra-1912 <saumitra.topinkatti@forty4hz.com>
2022-06-21 19:16:09 +0530

    refactor: code quality

--- Utils/Stats.cs
+++ Utils/Stats.cs
@@ -1,56 +1,56 @@
 ﻿namespace Minesweeper.Utils
 {
-	/// <summary>
+    /// <summary>
     /// Represents the Statistics for a Game object.
     /// </summary>
-	public class Stats
-	{
-		private int minesLive;
-		public int MinesLive
-		{
-			get { return minesLive; }
-		}
+    public class Stats
+    {
+        private int minesLive;
+        public int MinesLive
+        {
+            get { return minesLive; }
+        }
 
-		private int score;
-		public int Score
-		{
-			get { return Score; }
-		}
+        private int score;
+        public int Score
+        {
+            get { return Score; }
+        }
 
-		public Stats(int minesLive, int score)
-		{
-			this.minesLive = minesLive;
-			this.score = score;
-		}
+        public Stats(int minesLive, int score)
+        {
+            this.minesLive = minesLive;
+            this.score = score;
+        }
 
-		/// <summary>
+        /// <summary>
         /// Updates the MinesLive field value.
         /// </summary>
         /// <param name="minesLive">New value</param>
-		public void SetMinesLive(int minesLive)
-		{
-			this.minesLive = minesLive;
-		}
+        public void SetMinesLive(int minesLive)
+        {
+            this.minesLive = minesLive;
+        }
 
-		/// <summary>
+        /// <summary>
         /// Updates the Score field value.
         /// </summary>
         /// <param name="score">New value</param>
-		public void SetScore(int score)
-		{
-			this.score = score;
-		}
+        public void SetScore(int score)
+        {
+            this.score = score;
+        }
 
-		/// <summary>
+        /// <summary>
         /// Resets the MinesLive and Score fields.
         /// </summary>
         /// <param name="minesLive">Reset value for MinesLive field</param>
         /// <param name="score">Reset value for Score field</param>
-		public void Reset(int minesLive, int score)
-		{
-			this.minesLive = minesLive;
-			this.score = score;
-		}
-	}
+        public void Reset(int minesLive, int score)
+        {
+            this.minesLive = minesLive;
+            this.score = score;
+        }
+    }
 }
 
